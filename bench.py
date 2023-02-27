a825b418053dca1c5826b740462b5e6f615e8440
Daniel Gross <danielgross@Daniels-MacBook-Pro.local>
2023-02-27 05:43:39 -0800

    factor out visualization code, clean up formatting.

--- bench.py
+++ bench.py
@@ -7,11 +7,11 @@ import tqdm
 import time
 import random
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity # for testing
+from sklearn.metrics.pairwise import cosine_similarity  # for testing
 from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
-import openai 
+import openai
 import pandas as pd
 import plotly.express as px
 import tiktoken
@@ -23,7 +23,8 @@ import torch
 import re
 import dbm
 
-openai.api_key = open(os.path.expanduser('~/.openai')).read().strip() # Or however you want it.
+# Or however you want it.
+openai.api_key = open(os.path.expanduser('~/.openai')).read().strip()
 
 CLUSTER_COUNT = 10
 EMAIL_DATASET_COUNT = 10000
@@ -38,36 +39,47 @@ T5_TOKENIZER = T5Tokenizer.from_pretrained("t5-large")
 T5_EMBEDDING_CTX_LENGTH = 512
 
 _cache_dbm = dbm.open('cache.dbm', 'c')
-def list_disk_cache(namespace): # TODO Fix serialization so this function isn't so silly.
+
+
+# TODO Fix serialization so this function isn't so silly.
+def list_disk_cache(namespace):
     """Function decorator to cache function results to disk. Only for list items."""
     def decorator(func):
         def wrapper(*args, **kwargs):
-            key = hashlib.md5(str(args).encode() + str(kwargs).encode()).hexdigest()
+            key = hashlib.md5(str(args).encode() +
+                              str(kwargs).encode()).hexdigest()
             key = namespace + ':' + key
             if key in _cache_dbm:
                 return [float(x) for x in str(_cache_dbm[key])[3:-2].split(', ')]
             result = func(*args, **kwargs)
-            assert isinstance(result, list) # Don't be a meanie, I can only do lists!
+            # Don't be a meanie, I can only do lists!
+            assert isinstance(result, list)
             _cache_dbm[key] = str(result)
             return result
         return wrapper
     return decorator
 
+
 # Helper functions to lazy load various models.
 _t5_model = None
+
+
 def get_t5_model():
     global _t5_model
     if _t5_model is None:
-            from transformers import T5Model
-            print("Loading T5 model...")
-            model_name = "t5-large"
-            tokenizer = T5_TOKENIZER
-            model = T5Model.from_pretrained(model_name).cuda()
+        from transformers import T5Model
+        print("Loading T5 model...")
+        model_name = "t5-large"
+        tokenizer = T5_TOKENIZER
+        model = T5Model.from_pretrained(model_name).cuda()
 
-            _t5_model = (tokenizer, model)
+        _t5_model = (tokenizer, model)
     return _t5_model
 
+
 _st_model = None
+
+
 def get_sentence_tranformers(model):
     global _st_model
     if _st_model is None:
@@ -76,11 +88,15 @@ def get_sentence_tranformers(model):
         _st_model = SentenceTransformer(model)
     return _st_model
 
+
 def t5_encode(text):
-    tokens = T5_TOKENIZER.encode(text, return_tensors="pt", max_length=512, truncation=True)
+    tokens = T5_TOKENIZER.encode(
+        text, return_tensors="pt", max_length=512, truncation=True)
     return tokens.cuda() if CUDA_SUPPORT else tokens
-    
+
 # Helper functions to chunk larger inputs into smaller ones.
+
+
 def batched(iterable, n):
     """Batch data into tuples of length n. The last batch may be shorter."""
     # batched('ABCDEFG', 3) --> ABC DEF G
@@ -90,17 +106,20 @@ def batched(iterable, n):
     while (batch := tuple(islice(it, n))):
         yield batch
 
+
 def chunked_tokens(text, encoder_fn, chunk_length):
     tokens = encoder_fn(text)
     chunks_iterator = batched(tokens, chunk_length)
     yield from chunks_iterator
+
 
 def chunked_text(text, chunk_length, tokens_per_word=2.5):
     words = text.split(' ')
     chunks_iterator = batched(words, int(chunk_length / tokens_per_word))
     # when the we have a chunk of words, we join them back into a string
     yield from map(lambda chunk: ' '.join(chunk), chunks_iterator)
-    
+
+
 def get_long_embedding(text, embedding_fn, max_tokens=None, encoder_fn=None, average=True):
     assert max_tokens is not None
     assert encoder_fn is not None
@@ -111,32 +130,43 @@ def get_long_embedding(text, embedding_fn, max_tokens=None, encoder_fn=None, ave
         chunk_lens.append(len(chunk))
 
     if average:
-        chunk_embeddings = np.average(chunk_embeddings, axis=0, weights=chunk_lens)
-        chunk_embeddings = chunk_embeddings / np.linalg.norm(chunk_embeddings)  # normalizes length to 1
+        chunk_embeddings = np.average(
+            chunk_embeddings, axis=0, weights=chunk_lens)
+        chunk_embeddings = chunk_embeddings / \
+            np.linalg.norm(chunk_embeddings)  # normalizes length to 1
         chunk_embeddings = chunk_embeddings.tolist()
     return chunk_embeddings
 
 # Method 1: Get embeddings using T5 directly. # TODO: max pooling voodoo.
+
+
 def get_embedding_t5(text):
     tokenizer, model = get_t5_model()
     tokens = t5_encode(text)
     attn = tokens != tokenizer.pad_token_id
-    output = model.encoder(input_ids=tokens, attention_mask=attn, return_dict=True)
+    output = model.encoder(
+        input_ids=tokens, attention_mask=attn, return_dict=True)
     # Compute the mean of the last hidden state over the non-padded tokens. I think this is what they did in that paper, but I'm not sure...
-    embedding = (output.last_hidden_state * attn.unsqueeze(-1)).sum(dim=-2) / attn.sum(dim=-1)
+    embedding = (output.last_hidden_state * attn.unsqueeze(-1)
+                 ).sum(dim=-2) / attn.sum(dim=-1)
     return embedding.detach().cpu().numpy()[0]
 
 # Method 2: Use SentenceTransformers.
+
+
 def get_embedding_st(text, engine):
     model = get_sentence_tranformers(engine)
     if random.random() < 0.01:
         tokens = model.tokenize(text)['input_ids']
         sample_text = text[:100].replace('\n', ' ')
-        print(f"sample: len={len(text)}, num_tokens={len(tokens)}, max_len={model.max_seq_length}, text={sample_text}")
-        
+        print(
+            f"sample: len={len(text)}, num_tokens={len(tokens)}, max_len={model.max_seq_length}, text={sample_text}")
+
     return model.encode([text])[0]
 
 # Method 3: Use OpenAI's Embedding API
+
+
 @list_disk_cache("openai-embeddings")
 @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6), retry=retry_if_not_exception_type(openai.InvalidRequestError))
 def get_embedding_openai(text_or_tokens, model=OPENAI_EMBEDDING_MODEL):
@@ -149,7 +179,9 @@ def get_embedding_openai(text_or_tokens, model=OPENAI_EMBEDDING_MODEL):
         tokens = tokens[:OPENAI_EMBEDDING_CTX_LENGTH]
     return openai.Embedding.create(input=tokens, model=model)["data"][0]["embedding"]
 
-# Get embeddings. If "long_mode" is True, then we will chunk the input into smaller pieces and average the embeddings. 
+# Get embeddings. If "long_mode" is True, then we will chunk the input into smaller pieces and average the embeddings.
+
+
 def get_embeddings(text, engine, long_mode=False):
     max_tokens = None
     encoder_fn = None
@@ -174,13 +206,16 @@ def get_embeddings(text, engine, long_mode=False):
             return get_long_embedding(text, fn, max_tokens=max_tokens, encoder_fn=encoder_fn)
         elif engine == "t5":
             fn = get_embedding_t5
-            max_tokens = T5_EMBEDDING_CTX_LENGTH 
-            encoder_fn = get_long_embedding(text, fn, max_tokens=max_tokens, encoder_fn=encoder_fn)
+            max_tokens = T5_EMBEDDING_CTX_LENGTH
+            encoder_fn = get_long_embedding(
+                text, fn, max_tokens=max_tokens, encoder_fn=encoder_fn)
         elif engine.startswith("sentence-transformers/"):
             # TODO: I need to wrap SentenceTransformer in a subclass, that, when called, handle tokens_or_text, and not just text.
-            raise NotImplementedError("Long mode not implemented for SentenceTransformers")
+            raise NotImplementedError(
+                "Long mode not implemented for SentenceTransformers")
         else:
             raise ValueError(f"Unknown engine: {engine}")
+
 
 def download_dataset():
     dataset_link = "https://www.cs.cmu.edu/~./enron/enron_mail_20150507.tar.gz"
@@ -204,6 +239,7 @@ def get_all_files(path):
         all_files.extend(files)
     return all_files
 
+
 def get_emails(count=EMAIL_DATASET_COUNT):
     emails = []
     email_paths = get_all_files("data/maildir")
@@ -211,11 +247,13 @@ def get_emails(count=EMAIL_DATASET_COUNT):
     for file_name in email_paths:
         with open(file_name, "rb") as fp:
             try:
-                msg = email.message_from_binary_file(fp, policy=email.policy.default)
+                msg = email.message_from_binary_file(
+                    fp, policy=email.policy.default)
                 emails.append(msg)
             except:
                 pass
     return emails
+
 
 @retry(wait=wait_random_exponential(min=1, max=20), stop=stop_after_attempt(6), retry=retry_if_not_exception_type(openai.InvalidRequestError))
 def openai_completion(query):
@@ -227,6 +265,7 @@ def openai_completion(query):
         top_p=1,
         stop="Label:"
     )
+
 
 def get_label(cluster, labels, emails):
     # Get the indices of the emails in the cluster
@@ -242,20 +281,23 @@ def get_label(cluster, labels, emails):
     query = "The following are email subjects from the same cluster. Please provide a short label that describes the common theme or topic of the cluster.\n\n"
     for sample in samples:
         query += "- " + emails[sample]["subject"] + "\n"
-    
+
     query += "\nLabel:"
     # Call the OpenAI API
     response = openai_completion(query)
     # Return the label
     return response["choices"][0]["text"].strip()
 
+
 def plot_ploty(embeddings_2d, labels, labels_dict, file_name):
-    df = pd.DataFrame({"x": embeddings_2d[:, 0], "y": embeddings_2d[:, 1], "label": labels})
+    df = pd.DataFrame(
+        {"x": embeddings_2d[:, 0], "y": embeddings_2d[:, 1], "label": labels})
     df["label"] = df["label"].map(labels_dict)
     fig = px.scatter(df, x="x", y="y", color="label")
     fig.show()
     # save the image
     fig.write_image(file_name, width=1920, height=1080)
+
 
 def run_embedding_test(engine):
     download_dataset()
@@ -270,7 +312,8 @@ def run_embedding_test(engine):
         body = body.get_content() if body else ""
         if not body:
             continue
-        text = subject + "\n" + body # TODO: Should I use a separator token here? Who knows.
+        # TODO: Should I use a separator token here? Who knows.
+        text = subject + "\n" + body
         # text = re.sub(r'\s+', ' ', text) # Is this a good idea? Aren't newlines bad for embedding performance? Should test.
         embeddings.append(get_embeddings(text, engine))
     embeddings = np.array(embeddings)
@@ -292,7 +335,9 @@ def run_embedding_test(engine):
     file_name = f'{hashbit}-{engine_filename}-cluster{CLUSTER_COUNT}-email{EMAIL_DATASET_COUNT}'
     np.save(file_name + '-embeddings.npy', embeddings)
     plot_ploty(embeddings_2d, labels, labels_dict, file_name + '.png')
-    
+
+
 start_time = time.time()
-run_embedding_test('openai') # openai, sentence-transformers/all-mpnet-base-v2, sentence-transformers/gtr-t5-large (which should be T5)
+# openai, sentence-transformers/all-mpnet-base-v2, sentence-transformers/gtr-t5-large (which should be T5)
+run_embedding_test('openai')
 print("Time taken: ", time.time() - start_time)
---
+++ viz.py
@@ -0,0 +1,55 @@
+"""Tools to visualize embeddings."""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+
+
+def cluster(embeddings, labels, cluster_count=10):
+    """Cluster the embeddings and return the cluster labels."""
+    if not isinstance(embeddings, np.ndarray):
+        embeddings = np.array(embeddings)
+    kmeans = KMeans(n_clusters=cluster_count, random_state=42)
+    labels = kmeans.fit_predict(embeddings)
+    tsne = TSNE(n_components=2, random_state=42)
+    embeddings_2d = tsne.fit_transform(embeddings)
+    return embeddings_2d, labels
+
+
+def plot_matplotlib(embeddings_2d, labels, labels_dict):
+    """Embeddings 2D is the output of the TSNE function. 
+    Labels is the output of the KMeans function. 
+    Labels dict is a dictionary mapping the cluster number to the label.
+    """
+    fig, ax = plt.subplots(figsize=(15, 15))
+    ax.scatter(embeddings_2d[:, 0], embeddings_2d[:, 1],
+               c=labels, cmap="rainbow")
+    for cluster, label in labels_dict.items():
+        ax.plot([], [], label=label, marker="o", c=f"C{cluster}")
+    ax.legend(loc="center left", bbox_to_anchor=(1.05, 0.5), fontsize=14)
+    plt.show()
+
+
+def plot_ploty(embeddings_2d, labels, labels_dict):
+    """Same as above, but with Plotly."""
+    df = pd.DataFrame(
+        {"x": embeddings_2d[:, 0], "y": embeddings_2d[:, 1], "label": labels})
+    df["label"] = df["label"].map(labels_dict)
+    fig = px.scatter(df, x="x", y="y", color="label")
+    fig.show()
+
+
+def plot_3d_embeddings(embeddings, labels, labels_dict):
+    """Project into three dimensions.
+    Unlike prior functions, this one takes the embeddings straight up.
+    """
+    tsne = TSNE(n_components=3, random_state=42)
+    embeddings_3d = tsne.fit_transform(embeddings)
+    df = pd.DataFrame(
+        {"x": embeddings_3d[:, 0], "y": embeddings_3d[:, 1], "z": embeddings_3d[:, 2], "label": labels})
+    df["label"] = df["label"].map(labels_dict)
+    fig = px.scatter_3d(df, x="x", y="y", z="z", color="label")
+    fig.show()
